698ba4b50e054c4c41ae9c2b89ad70dc8285c981
Sheikh Hefzul Bari <67674490+shbari@users.noreply.github.com>
2024-02-06 17:14:15 +0900

    Minor update

--- Normal_distribution_plot.py
+++ Normal_distribution_plot.py
@@ -18,10 +18,10 @@ fig, ax = plt.subplots()
 # Set axis to top and bottom only
 ax.spines[['right', 'top']].set_visible(False)
 # plot first curve (let say low flow) starting from 2.1 and interval 1.5
-plt.plot(x, norm.pdf(x, 2.1, 1.5))
+plt.plot(x, norm.pdf(x, 1.90, 1.5))
          #, label="Low flow")
 #plot second curve (let say high flow)
-plt.plot(y, norm.pdf(y, 2.5, 1.5))
+plt.plot(y, norm.pdf(y, 3.2, 1.5))
          #, label= 'High flow')
 # plot legends with font size 17, position upper left corner
 #plt.legend(["Low Flow", "High Flow"], fontsize="17", loc ="upper left")
@@ -29,18 +29,19 @@ plt.plot(y, norm.pdf(y, 2.5, 1.5))
 plt.tick_params(labelleft=False, left=False)
 plt.tick_params(labelbottom=False, bottom=False)
 # plot x and y axis level,  font size 28
-#plt.ylabel('Fraction (%)', fontsize=28)
-#plt.xlabel('Diameter (µm)', fontsize=28)
+plt.ylabel('Fraction (%)', fontsize=28)
+plt.xlabel('Diameter (µm)', fontsize=28)
 # get the legend object
 leg = ax.legend()
 # change the line width for the legend
 #for line in leg.get_lines():
    # line.set_linewidth(2.0)
-#plt.legend(["Low Flow", "High Flow"], fontsize="27.5", loc = "upper left", handlelength=0.5, handletextpad=0.1, frameon=False)
+#plt.legend(["Low Flow", "High Flow"], fontsize="27", loc = "upper left", handlelength=0.5, handletextpad=0.1, frameon=False)
+plt.legend(["Low Flow", "High Flow"], fontsize="27", loc = "upper left", bbox_to_anchor=(-0.05, 1), handlelength=0.5, handletextpad=0.1, frameon=False)
 #plt.xlim([-5, 5])
-#plt.savefig('figure5a.pdf')
+plt.savefig('figure5a.pdf')
 plt.show()
-
+#plt.savefig('5a.pdf', format='pdf')
 
 # In[ ]:
 
