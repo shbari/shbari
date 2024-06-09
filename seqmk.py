e146a2d893d97de37e78230c04be954709618c04
Sheikh Hefzul Bari <67674490+shbari@users.noreply.github.com>
2024-06-10 00:27:58 +0900

    Revert "Revert "Continuous MK test""
    This reverts commit 1c634acf072a6af9991f376bdae1f37353747920.

--- seqmk.py
+++ seqmk.py
@@ -1,7 +1,10 @@
-import numpy as np
-from scipy.stats import kendalltau
 # This is a translted version from R package pheno.
 # This will compute a series of kendall's tau as described by Sneyers(1990).
+# https://rdrr.io/cran/pheno/src/R/seqMK.R
+import numpy as np
+from scipy.stats import kendalltau
+import pandas as pd
+
 def seqMK(x):
     n = len(x)
     y = x[::-1]
@@ -13,6 +16,8 @@ def seqMK(x):
     if n < 2:
         raise ValueError("seqMK: not enough finite observations")
     # progressive and retrograde series
+    '''In Python, the range(start, stop) function generates a sequence of integers 
+    starting from start up to, but not including, stop.'''
     for i in range(1, n):
         prog[i] = kendalltau(x[:i+1], range(i+1))[0]
         retr[i] = kendalltau(y[:i+1], range(i+1))[0]
@@ -24,13 +29,15 @@ def seqMK(x):
             tp[i+1] = False
         else:
             tp[i+1] = True
-    return {'prog': prog, 'retr': retr, 'tp': tp}
+    # Create a DataFrame from the results
+    result_df = pd.DataFrame({'prog': prog, 'retr': retr, 'tp': tp})
+    # Save the DataFrame to a CSV file
+    result_df.to_csv('result.csv', index=False)
+    return result_df
 
 # Your time series data
 x = np.array([1, 2, 3, 2, 3, 4, 5, 4, 5, 6])
 
 # Apply the seqMK function
 result = seqMK(x)
-
-# Print the result
 print(result)
